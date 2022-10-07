d3e0252458a80dbed1892d42576883127b4734b2
Kauanator <kauavitorloiola@gmail.com>
2022-10-06 21:43:17 -0300

    fazer um menu de hamburguer

--- home.php
+++ home.php
@@ -5,14 +5,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="Cache-Control" content="no-cache" />
-    <link rel="stylesheet" href="./style/home.css">
+    <link rel="stylesheet" href="./view/tema/style/home.css">
     <title>Menu</title>
 </head> 
     <header>
         <div class="container_logo">
             <div class="logo">
                 <a href="home.php">
-                <img src="./images/logo.svg" width="90" height="90">
+                <img src="./media/images/logo.svg" width="90" height="90">
                 </a>
                 <h1>AllInstruments</h1>
             </div>
@@ -23,9 +23,12 @@
         <div class="menu">
             <nav>
                 <ul>
-                    <a href="#"><p>Produtos</p></a>
-                    <a href="#"><p>Sobre</p></a>
-                    <a href="cadastro.php"><p>Cadastrar-se</p></a>
+                    <a href="controller/produtos.php"><P>Produtos</P></a><br>
+                    <a href="controller/clientes.php"><p>Clientes</p></a><br>
+                    <a href="controller/contato.php"><p>Contato</p></a><br>
+                    <a href="controller/sobre.php"><p>Sobre</p></a><br>
+                    <a href="controller/conta.php"><p>Conta</p></a><br>
+                    <a href="controller/carrinho.php"><p>Carrinho</p></a><br> 
                 </ul>
             </nav>
        </div>
