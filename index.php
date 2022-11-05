4098f487010223768a386db70ec4e0a36646a3a6
GabrielSSoouza <79284046+GabrielSSoouza@users.noreply.github.com>
2022-11-05 14:47:23 -0300

    Update index.php
    .

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="Cache-Control" content="no-cache" />
-    <link rel="stylesheet" href="./view/tema/style/home.css">
+    <link rel="stylesheet" href="./view/tema/style/index.css">
     <title>Inicio</title>
 </head> 
     <header>
