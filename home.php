e8d99b60540e7bafe18dd1180beecc369984df1d
Kauanator <kauavitorloiola@gmail.com>
2022-11-06 00:41:23 -0300

    tirando a barra antiga: fazer uma mais simples.

--- home.php
+++ home.php
@@ -13,42 +13,38 @@ include('./controller/conectar.php');
     <meta http-equiv="Cache-Control" content="no-cache" />
     <link rel="stylesheet" href="./view/tema/style/home.css">
     <title>Menu</title>
-</head> 
-    <header>
-        <div class="container_logo">
-            <div class="logo">
-                <a href="home.php">
-                <img src="./view/imagens/logo.svg" width="90" height="90">
-                </a>
-                <h1>AllInstruments</h1>
-            </div>
+</head>
+<header>
+    <div class="container_logo">
+        <div class="logo">
+            <a href="home.php">
+            <img src="./view/imagens/logo.svg" width="90" height="90">
+            </a>
+            <h1>AllInstruments</h1>
         </div>
-            <div class="search-box">
-                <input type="text" name="search" id="" class="search-txt" placeholder="Pesquisar">
-                <a href="#" class="search-btn">
-                    <img src="./view/imagens/loupe.svg" alt="Pesquisar">
-                </a>
-            </div>
-        <div class="menu">
-            <nav>
-                <ul>
-                    <a href="controller/produtos.php"><P>Produtos</P></a>
+    </div>
+    <div class="menu">
+        <nav>
+            <ul>
+                <a href="controller/produtos.php"><P>Produtos</P></a>
 
-                    <a href="controller/clientes.php"><p>Clientes</p></a>
+                <a href="controller/clientes.php"><p>Clientes</p></a>
 
-                    <a href="controller/contato.php"><p>Contato</p></a>
+                <a href="controller/contato.php"><p>Contato</p></a>
 
-                    <a href="controller/sobre.php"><p>Sobre</p></a>
+                <a href="controller/sobre.php"><p>Sobre</p></a>
 
-                    <img src="" alt=""><a href="controller/conta.php"><p>Perfil</p></a></img> <!-- icone de perfil -->
-                    <a href="controller/carrinho.php"><p>Carrinho</p></a> 
-                </ul>
-            </nav>
-       </div>
-    </header>
-    <body>
-        <?php 
-        
-        ?>
-    </body>
+                <img src="" alt=""><a href="controller/conta.php"><p>Perfil</p></a></img> <!-- icone de perfil -->
+                <a href="controller/carrinho.php"><p>Carrinho</p></a> 
+            </ul>
+        </nav>
+    </div>
+</header>
+<body>
+    <h1>lista de produtos</h1>
+    <form action="" method="GET">
+        <input name="busca" placeholder="Pesquisar" type="text">
+        <button type="submit">Pesquisar</button>
+    </form>
+</body>
 </html>
