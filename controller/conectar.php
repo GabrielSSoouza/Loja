81b545decce244697056fb4bbdd0870a6df51352
Vitor Kauã <kauavitorloiola@gmail.com>
2022-12-11 13:15:41 -0300

    mudando a forma de conexão para PDO

--- controller/conectar.php
+++ controller/conectar.php
@@ -1,10 +1,10 @@
 <?php
-$host = "localhost";
-$db = "fmmkayoa_allinstruments";
-$user = "fmmkayoa_allinstruments";
-$pass = "allinstruments";
+$conexao = new PDO("mysql:host=localhost;dbname=fmmkayoa_allinstruments", "fmmkayoa_allinstruments", "allinstruments");
+$conexao->exec("SET CHARACTER SET utf8");
 
-if($mysqli->error) {
-    die("Falha ao conectar o banco de dados: " . $mysqli->error);
+try {
+    $pdo = $conexao;
+} catch (PDOException $e) {
+    echo $e->getMessage;
 }
 ?>
