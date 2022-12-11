b04b96e89d1db7ef8969897ad5bad446e3283dbd
Vitor Kauã <kauavitorloiola@gmail.com>
2022-12-11 13:03:11 -0300

    esclarecendo o sql_code

--- controller/process.php
+++ controller/process.php
@@ -6,13 +6,12 @@ $nome = $_POST['nome'];
 $email = $_POST['Email'];
 $senha = $_POST['pass'];
 
-$sql = "insert into usuarios (nome, email, senha) values ('$nome', '$email', '$senha')";
+$sql = "INSERT INTO usuarios (nome, email, senha) VALUES ('$nome', '$email', '$senha')";
 
-$conn = new mysqli("localhost", "fmmkayoa_allinstruments", "allinstruments", "fmmkayoa_allinstruments");
-
+$conn = new mysqli('localhost', 'fmmkayoa_allinstruments', 'allinstruments', 'fmmkayoa_allinstruments');
 $conn-> query($sql);
 
 $conn->close();
 
-echo 'sucesso';
+echo 'cadastrado com sucesso!';
 ?>
