67ce17a32ebb62d0c6f260e7859dc7e979a2cb89
Vitor Kauã <kauavitorloiola@gmail.com>
2022-12-12 21:17:56 -0300

    protótipo do carrinho

--- controller/carrinho.php
+++ controller/carrinho.php
@@ -1,6 +1,6 @@
 <?
-
 include('protect.php');
+session_start()
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -8,14 +8,43 @@ include('protect.php');
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <!-- Basic -->
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <!-- Mobile Metas -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <!-- Site Metas -->
+  <meta name="keywords" content="" />
+  <meta name="description" content="" />
+  <meta name="author" content="" />
+
+  <title>Produtos</title>
+
+  <!-- slider stylesheet -->
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.1.3/assets/owl.carousel.min.css" />
+
+  <!-- bootstrap core css -->
+  <link rel="stylesheet" type="text/css" href="../view/css/bootstrap.css" />
+
+  <!-- fonts style -->
+  <link href="https://fonts.googleapis.com/css?family=Baloo+Chettan|Dosis:400,600,700|Poppins:400,600,700&display=swap" rel="stylesheet" />
+  <!-- Custom styles for this template -->
+  <link href="../view/css/style.css" rel="stylesheet" />
+  <!-- responsive style -->
+  <link href="../view/css/responsive.css" rel="stylesheet" />
     <title>Carrinho</title>
 </head>
 <body>
 
-    <h1>Essa é a página de carrinho do site</h1>
+<h2>Carrinho</h2>
 
-    carrinho
-
+    <div class="box" id="carrino-container">
+        <div </div> class="produto">
+            <img src="<?php echo $row['imagem']?>">
+            <a href="?acicionar=<?php echo $row['id_produto']?>">adicionar ao carrinho</a>
+       
+    </div>
+    
     <p>
         <a href="../index.php">Inicio</a>
     </p>
